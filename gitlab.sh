b1dfaf459758339166d53ffc0f2f5222d84d04ac
Ben Chen <bn@benbenbang.tw>
2016-05-25 18:03:03 +0800

    Add if-else

--- gitlab.sh
+++ gitlab.sh
@@ -1,16 +1,19 @@
-# bash gitlabFT.sh for First time clone 
-# git clone --mirror git@gitlab.com:username/repo.git
-# cd repo.git
-# git push --no-verify --mirror git@github.com:username/repo.git
-# git remote set-url --push origin git@github.com:username/repo.git
-# git fetch -p origin
-# git push --no-verify --mirror
-# cd ..
-
-# bash gitlab.sh
-cd repo.git
-git push --no-verify --mirror git@github.com:username/repo.git
-git remote set-url --push origin git@github.com:username/repo.git
-git fetch -p origin
-git push --no-verify --mirror
-cd ..
+if [ ! -e repo.git ]
+then
+  # bash gitlabFT.sh for First time clone 
+  git clone --mirror git@gitlab.com:username/repo.git
+  cd repo.git
+  git push --no-verify --mirror git@github.com:username/repo.git
+  git remote set-url --push origin git@github.com:username/repo.git
+  git fetch -p origin
+  git push --no-verify --mirror
+  cd ..
+else
+  # bash gitlab.sh
+  cd repo.git
+  git push --no-verify --mirror git@github.com:username/repo.git
+  git remote set-url --push origin git@github.com:username/repo.git
+  git fetch -p origin
+  git push --no-verify --mirror
+  cd ..
+fi
