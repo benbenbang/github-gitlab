2a3b5cf6532b58fe76394b302cb049dcd3998dba
Ben Chen <bn@benbenbang.tw>
2016-05-25 16:05:27 +0800

    Update gitlab.sh

--- gitlab.sh
+++ gitlab.sh
@@ -1,4 +1,7 @@
+git clone --mirror git@your-gitlab-site.com:username/repo.git
 cd repo.git
+git push --no-verify --mirror git@github.com:username/my-repo.git
+git remote set-url --push origin git@github.com:username/my-repo.git
 git fetch -p origin
 git push --no-verify --mirror
 cd ..
