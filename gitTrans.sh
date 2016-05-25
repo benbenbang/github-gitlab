a75056e962858e0c3bd40e4b5fb8ac55a5916baa
Ben Chen <bn@benbenbang.tw>
2016-05-25 18:44:16 +0800

    Update gitTrans.sh

--- gitTrans.sh
+++ gitTrans.sh
@@ -4,21 +4,25 @@ toGit="git@gitlab.com"
 username="username"
 repo="repo.git"
 
+# Automatically check and do our job.
+# Don't have to change anything.
 if [ ! -e "$repo" ]
 then
   # bash to clone the repo.git for the First time usage 
-  git clone --mirror "$fromGit":"$userName"/"$repo"
+  echo "We're now preparing to clone the git from $fromGit to $toGit"
+  git clone --mirror "$fromGit":"$username"/"$repo"
+  echo "Done."
   cd "$repo"
-  git push --no-verify --mirror "$toGit":"$userName"/"$repo"
-  git remote set-url --push origin "$toGit":"$userName"/"$repo"
+  echo "Prepare to push to $toGit"
+  git push --no-verify --mirror "$toGit":"$username"/"$repo"
+  git remote set-url --push origin "$toGit":"$username"/"$repo"
   git fetch -p origin
   git push --no-verify --mirror
   cd ..
 else
   # bash to push to toGit
   cd "$repo"
-  git push --no-verify --mirror "$toGit":"$userName"/"$repo"
-  git remote set-url --push origin "$toGit":"$userName"/"$repo"
+  echo "Ready to push to $toGit"
   git fetch -p origin
   git push --no-verify --mirror
   cd ..
